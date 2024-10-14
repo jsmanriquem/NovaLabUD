e0a92f168934d798fd6d63c5b571b50d609a14a4
jsmanriquem <jsebastianmanrique@gmail.com>
2024-10-14 00:28:47 -0500

    Creando pcarpeta inicial de la documentación

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Andrés Gómez, Jorge Garzón, Julián Aros, Laura Triana, Laura Oliveros, Sebastian Manrique'
+copyright = '2024, proyecto_final'
+author = 'proyecto_final'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
