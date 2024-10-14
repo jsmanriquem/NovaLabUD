65e6ef4780f46ef88b02c0851a80c2024aeeb092
georgfis <161676015+georgfis@users.noreply.github.com>
2024-10-14 00:04:16 -0500

    Update regression_analysis.py
    avance clase para análisis de regresiones

--- regression_analysis.py
+++ regression_analysis.py
@@ -6,3 +6,15 @@ from sklearn.linear_model import LinearRegression
 from tkinter import simpledialog, messagebox
 import tkinter as tk
 
+class RegressionAnalysis:
+    """Clase para realizar análisis de regresión.
+
+    Esta clase proporciona métodos para realizar análisis de regresión lineal
+    y polinómica, así como interpolación de datos. Las métricas de desempeño
+    se pueden calcular y mostrar en una interfaz gráfica.
+
+    Attributes:
+        data_ops (DataOperations): Objeto que contiene los datos necesarios
+                                    para el análisis de regresión.
+    """
+  
