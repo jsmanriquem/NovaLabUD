c1ae1a255d67cbbd21baff539a4a18e66647daee
fernandogomez0621 <62316218+fernandogomez0621@users.noreply.github.com>
2024-10-13 23:21:53 -0500

    Update regression_analysis.py
    Librerias necesarias

--- regression_analysis.py
+++ regression_analysis.py
@@ -1,1 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from tkinter import simpledialog, messagebox
+import tkinter as tk
 
