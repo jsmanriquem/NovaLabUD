33ccd4fc1fa0ea2dc89a32728384b2ffb7a492b4
jsmanriquem <jsebastianmanriquem@gmail.com>
2024-10-13 23:00:29 -0500

    Añadiendo la documentación inicial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,20 @@
 
 import os
 import sys
+
+import sphinx_book_theme
+
+# Configurar el tema
+html_theme = 'sphinx_book_theme'
+
+# Opciones del tema (puedes personalizarlo según tus necesidades)
+# html_theme_options = {
+#     # "repository_url": "https://github.com/tu-repositorio",  # Enlace a tu repositorio
+#     "use_repository_button": True,
+#     "use_edit_page_button": True,  # Mostrar botón para editar en GitHub
+#     "use_issues_button": True,     # Mostrar botón para reportar problemas
+# }
+
 sys.path.insert(0, os.path.abspath('../../code'))
 
 project = 'toma_datos'
@@ -18,7 +32,7 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,5 +42,5 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 html_static_path = ['_static']
