ab83a7c8d7c45a9bbf52a415531a58457f0e4d6e
jsmanriquem <jsebastianmanriquem@gmail.com>
2024-11-14 23:05:53 -0500

    Modificando archivo conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,32 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Paquetería importante
+import os
+import sys
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Instalación del tema principal
+import sphinx_book_theme
 
-project = 'Graficador'
-copyright = '2024, Laura Oliveros & Laura Triana'
-author = 'Laura Oliveros & Laura Triana'
+# Ruta a la carpeta con el código a documentar
+sys.path.insert(0, os.path.abspath('../code'))
+
+# Nombre del proyecto
+project = 'Proyecto final'
+# Copyright
+copyright = '2024, Andrés Gómez, Jorge Garzón, Julián Aros, Laura Oliveros, Laura Triana, Sebastian Manrique'
+# Autor del proyecto
+author = 'Andrés Gómez, Jorge Garzón, Julián Aros, Laura Oliveros, Laura Triana, Sebastian Manrique'
+# Versión del proyecto
 release = '1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Extensiones de la documentación
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
 
-extensions = ['sphinx.ext.autodoc']
-
+# Ruta para carpeta de plantillas
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Lenguaje de la documentación
 language = 'es'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'furo'
+# Tema de la documentación
+html_theme = 'sphinx_book_theme'
+# Ruta para carpeta de archivos estáticos
 html_static_path = ['assets']
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../code'))
